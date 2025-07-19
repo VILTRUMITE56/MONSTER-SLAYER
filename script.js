29b6e30ff404c0bbbbba6560242d480ae811e225
Gabriel Ybanez <ybagabrielfrancis@gmail.com>
2025-07-20 00:44:09 +0800

    Update script.js

--- script.js
+++ script.js
@@ -18,10 +18,41 @@ const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
 
+const locations = [
+  {
+    name: "town square",
+    "button text": ["Go to store", "Go to cave", "Fight dragon"],
+    "button functions": [goStore, goCave, fightDragon],
+    text: 'You are in the town square. You see a sign that says "Store".',
+  },
+  {
+    name: "store",
+    "button text": [
+      "Buy 10 health (10 gold)",
+      "Buy weapon (30 gold)",
+      "Go to town square",
+    ],
+    "button functions": [buyHealth, buyWeapon, goTown],
+    text: "You enter the store.",
+  },
+];
+
 // initialize buttons
 button1.onclick = goStore;
 button2.onclick = goCave;
 button3.onclick = fightDragon;
+
+function goTown() {
+  button1.innerText = "Go to store";
+  button2.innerText = "Go to cave";
+  button3.innerText = "Fight dragon";
+
+  button1.onclick = goStore;
+  button2.onclick = goCave;
+  button3.onclick = fightDragon;
+  text.innerText =
+    'You are in the town square. You see a sign that says "Store".';
+}
 
 function goStore() {
   button1.innerText = "Buy 10 health (10 gold)";
@@ -41,4 +72,5 @@ function fightDragon() {
 }
 function buyHealth() {}
 function buyWeapon() {}
-function goTown() {}
+
+function update(location) {}
