64fad70dfc5b37301e6bebd0cfdd8bc49543ccac
Gabriel Ybanez <ybagabrielfrancis@gmail.com>
2025-07-22 02:44:41 +0800

    Update script.js

--- script.js
+++ script.js
@@ -97,7 +97,7 @@ function buyHealth() {
   }
 }
 function buyWeapon() {
-  if (currentWeaponIndex < 3) {
+  if (currentWeaponIndex < weapons.length-1) {
     if (gold >= 30) {
       gold -= 30;
       currentWeaponIndex++;
@@ -110,6 +110,8 @@ function buyWeapon() {
     } else {
       text.innerText = "You do not have enough gold to buy a weapon.";
     }
+  }else {
+      text.innerText = "You already have the most powerful weapon!";
   }
 }
 
