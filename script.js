1ed064dbe53d88d2c38cecee0f5b49bc9ed007b5
Gabriel Ybanez <ybagabrielfrancis@gmail.com>
2025-07-26 01:56:49 +0800

    Update script.js

--- script.js
+++ script.js
@@ -204,8 +204,13 @@ function attack() {
   text.innerText +=
     " You attack it with your " + weapons[currentWeaponIndex].name + ".";
   health -= getMonsterAttackValue(monsters[fighting].level);
-  monsterHealth -=
-    weapons[currentWeaponIndex].power + Math.floor(Math.random() * xp) + 1;
+  if (isMonsterHit()) {
+    monsterHealth -=
+      weapons[currentWeaponIndex].power + Math.floor(Math.random() * xp) + 1;
+  } else {
+    text.innerText += " You miss.";
+  }
+
   healthText.innerText = health;
   monsterHealthText.innerText = monsterHealth;
   if (health <= 0) {
@@ -223,6 +228,10 @@ function getMonsterAttackValue(level) {
   const hit = level * 5 - Math.floor(Math.random() * xp);
   console.log(hit);
   return hit > 0 ? hit : 0;
+}
+
+function isMonsterHit() {
+  return Math.random() > 0.2;
 }
 
 function dodge() {
